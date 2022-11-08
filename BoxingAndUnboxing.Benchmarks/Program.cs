131e1f56ceb7c085c63c8a2e5bdc132b1e40fd55
David Gómez <ldavid.gomez@gmail.com>
2022-11-08 10:29:58 +0100

    Add Boxing and Unboxing files :)

---
+++ BoxingAndUnboxing.Benchmarks/Program.cs
@@ -0,0 +1,54 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BoxingAndUnboxing.Benchmarks
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchy>();
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class Benchy
+    {
+        private static readonly BoxingService BoxingService = new();
+        private static readonly object Boxed = 420;
+
+        [Benchmark]
+        public object BoxValue() => BoxingService.BoxValue(420);
+
+        [Benchmark]
+        public int UnboxValue() => BoxingService.UnboxValue(Boxed);
+        [Benchmark]
+        public int SimpleReturn() => BoxingService.SimpleReturnInt(420);
+
+        [Benchmark]
+        public object SimpleReturnObject() => BoxingService.SimpleReturnObject(Boxed);
+    }
+    
+    public class BoxingService
+    {
+        public object BoxValue(int number)
+        {
+            return number;
+        }
+
+        public int UnboxValue(object value)
+        {
+            return (int)value;
+        }
+
+        public int SimpleReturnInt(int number)
+        {
+            return number;
+        }
+
+        public object SimpleReturnObject(object value)
+        {
+            return value;
+        }
+    }
+}
---
+++ BoxingAndUnboxing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        // int someNumber = 420; 
+        // object someNumberObject = someNumber; //boxing
+        // int unbox = (int)someNumberObject; // unboxing
+
+        var arrayOfInts = Enumerable.Range(69, 420).ToArray();
+        var arrayList = new ArrayList(arrayOfInts);
+        var list = new List<int>(arrayOfInts);
+
+    }
+}
